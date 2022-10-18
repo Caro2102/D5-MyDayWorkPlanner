91fa874e05489654e66d2d8adab9b0b981acbc72
Carolina Cruz <caro21_cruz@hotmail.com>
2022-10-18 14:56:11 -0500

    Se agrego funcion para que se hicieran filas a cada una hora del array

--- script.js
+++ script.js
@@ -12,3 +12,45 @@ var crteTb =$("<tbody>");
 //Se generan en el html
 $(".containe").append(crteTable);
     crteTable.append(crteTb);
+    
+function schedule(){
+    save(m);
+        for(var i=0; i<horas.length; i++) {
+            //Hora actual formato 24hrs
+            var currentT=moment();
+            var actualT=[];
+                actualT.push(currentT.format('HHA'))
+            //A cada una de las horas del array se ponen en formato 24hrs
+            var current= moment().hour(horas[i]);
+            var newTime=[];
+                newTime.push(current.format('HHA'))
+                
+            var crteTr =$("<tr>");
+            //Se crea un td para colocar las horas
+            var crteTdH =$("<td>");
+                crteTdH.addClass('hour col-sm-1');
+                crteTdH.text(newTime)
+                eventH=crteTdH.text();
+            //Se crea td para colocar textArea
+            var crteTdE =$("<td>");
+                crteTdE.addClass('event-day col-lg-11');
+            //Se crea text Area
+            var crteTextA=$("<textarea>");
+                crteTextA.addClass('row ');
+            //Se crea td que funciona como boton
+            var crteTdB =$("<td>");
+                crteTdB.addClass('saveBtn col-lg-2');
+            //Se agrega icono guardar
+            var icon = $("<i>");
+                icon.addClass("far fa-save");
+
+        //A cada una de las horas del array se le va a generar una fila con hora, text area y boton
+        crteTb.append(crteTr);
+        crteTr.append(crteTdH);
+        crteTr.append(crteTdE);
+        crteTdE.append(crteTextA)
+        crteTr.append(crteTdB);
+        crteTdB.append(icon);
+
+    }
+}
